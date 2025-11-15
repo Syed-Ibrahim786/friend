b284424f4598df348ce5f0da8417164dbc933d4c
LIJIN <93189213+Lijin-18@users.noreply.github.com>
2025-11-15 19:25:29 +0530

    Add files via upload

---
+++ Friend/src/main/java/Friend_proto/example/Friend/FriendApplication.java
@@ -0,0 +1,13 @@
+package Friend_proto.example.Friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendApplication.class, args);
+	}
+
+}
---
+++ Friend/src/test/java/Friend_proto/example/Friend/FriendApplicationTests.java
@@ -0,0 +1,13 @@
+package Friend_proto.example.Friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
